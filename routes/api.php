2c966275bf66ce0f5f037573f6693b52e8206a9b
Sai Pone Tha Aung <saiponethaaung@gmail.com>
2018-02-14 01:16:25 +0630

    Add drs api test case

--- routes/api.php
+++ routes/api.php
@@ -34,3 +34,19 @@ Route::group(['prefix' => 'seaware/test'], function(){
     Route::get('OTA_CruiseBookRQ', 'Test\\SeawareApiTest@otaCruiseBookRQ');
     Route::get('OTA_CruiseBookingDocumentRQ', 'Test\\SeawareApiTest@otaCruiseBookingDocumentRQ');
 });
+
+Route::group(['prefix' => 'drs/test'], function() {
+    Route::get('API_AutoUA_APA_Currency_OvrTooMuchFlag', 'Test\\DrsApiTestController@acnillaryPointAdjustment');
+    Route::get('API_AutoUA_COMP_V2 ', 'Test\\DrsApiTestController@apiAutoUACompV2');
+    Route::get('API_AutoUA_GetPF', 'Test\\DrsApiTestController@apiAutoUAGetPF');
+    Route::get('API_AutoUA_GetSelectedPF', 'Test\\DrsApiTestController@apiAutoUAGetSelectedPF');
+    Route::get('API_AutoUA_Get_CustomerProfile_Format_Long', 'Test\\DrsApiTestController@apiAutoUAGetCustomerProfileFormatLong');
+    Route::get('API_AutoUA_PINChange', 'Test\\DrsApiTestController@apiAutoUAPinChange');
+    Route::get('API_AutoUA_PINCreate', 'Test\\DrsApiTestController@apiAutoUAPinCreate');
+    Route::get('API_AutoUA_PINVerify', 'Test\\DrsApiTestController@apiAutoUAPinVerify');
+    Route::get('API_AutoUA_SetPF', 'Test\\DrsApiTestController@apiAutoUASetPF');
+    Route::get('API_EditCustomerV2', 'Test\\DrsApiTestController@apiEditCustomerV2');
+    Route::get('API_NewCustomer_V3', 'Test\\DrsApiTestController@apiNewCustomerV1');
+    Route::get('API_Report_CSR', 'Test\\DrsApiTestController@apiReportCsrV2');
+    Route::get('API_Report_CTSv2', 'Test\\DrsApiTestController@apiReportCtsV2');
+});
